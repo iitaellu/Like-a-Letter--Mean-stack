a46c642fce0916840bbd41a873adbc529a784c22
me mee <me@email.com>
2023-05-09 20:56:07 +0300

    some backend and a little frontend

---
+++ models/messages.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const config = require('../config/database');
+//const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+let messageSchema = new Schema({
+    person1: String,
+    person2: String,
+    msg: []
+})
+
+module.exports = mongoose.model("Message", messageSchema)
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const config = require('../config/database');
 const User = require('../models/user');
+const Message = require('../models/messages');
 const passport = require('passport');
 
 //Register
@@ -62,5 +63,86 @@ router.post('/authenticate', (req, res, next) => {
 router.get("/profile", passport.authenticate('jwt', {session:false}), (req, res, next)=>{
     res.json({user: req.user});
 });
+
+// messages
+router.get("/messages",passport.authenticate('jwt', {session:false}), (req, res, next)=>{
+    console.log(req)
+    
+    
+    res.json({user: req.user});
+});
+
+//after cliking message
+router.get('/readMessages', (req, res, next)=>{
+    const id = req.body.messageID;
+    Message.findById(id, (err, message) => {
+        if(err) throw err;
+        if(message){
+            Message.findById(req.body.messageID, (err, doc)=> {
+                if(err) throw err;
+                let messages = [];
+                for (let i = 0; i < doc.msg.length; i++){
+                    if (doc.msg[i][1] == message.patient){
+                        oneMessage = "P: "+ doc.msg[i][2];
+                        messages.push(oneMessage);
+                        if(doc.msg[i][3] = false){
+                            doc[i][3] = true
+                        }
+                    }
+                    if(doc.msg[i][1] == message.doctor){
+                        oneMessage = "D: "+ doc.msg[i][2];
+                        messages.push(oneMessage);
+                        if(doc.msg[i][3] = false){
+                            doc[i][3] = true
+                        }
+                    }
+                }
+                res.json({message: messages})                
+            })
+        }
+        else{
+            return res.json({success: false, msg: 'messages not found'});
+        }
+    })
+});
+
+//Send messsage in old message
+router.post('/sendMessage', (req, res, next) => {
+
+    const id = req.body.messageID;
+    Message.findById(id, (err, message) => {
+        if(err) throw err;
+        if(message){
+            msg = message.msg;
+
+            newMsg= [msg.length, req.body.sender, req.body.message, false]
+            msg.push(newMsg)
+            //msg.add(req.body.message)
+            //FROM https://www.youtube.com/watch?v=qrDlIiq9zAc
+            Message.findByIdAndUpdate(req.body.messageID, {$set:{ msg: msg}}, (err, doc)=> {
+                if(err) throw err;
+                res.json(doc)
+            })
+        }
+        else{
+            return res.json({success: false, msg: 'messages not found'});
+        }
+    })
+})
+
+//SEND NEW MESSAGE'
+router.post('/sendNewMessage', (req, res, next) => {
+
+    msg = [0, req.body.sneder, req.body.msg];
+
+    let newMessage = new Message ({
+        person1: req.body.sender,
+        person2: req.body.recipient,
+        msg: [msg]
+    })
+
+    newMessage.save();
+    res.json({msg: "Message sent"});
+})
 
 module.exports = router;
